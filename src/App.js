dc2c7d80ec97f8024f8fefba338c2a9b26571c5c
AhmedReFu <mdnazmulkhanfeni@gmail.com>
2022-05-01 01:26:32 +0600

    added onAuthState Change to parsist the auth

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home/Home';
 import './App.css';
 import Login from './components/Login/Login';
 import Header from './components/Header/Header';
+import Register from './components/Register/Register';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
       </Routes>
     </div>
   );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase({});
     return (
         <div>
             <h3>Please Login</h3>
+            <div style={{ margin: '20px' }}>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+            <form action="">
+                <input type="email" placeholder='your email' />
+                <br />
+                <input type="password" placeholder='your password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
